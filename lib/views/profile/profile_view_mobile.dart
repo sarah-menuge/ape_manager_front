add2e2b20324eb5343db03f34d0c733e6a76b834
florian_robe <florian_robe@ens.univ-artois.fr>
2024-02-19 17:31:26 +0100

    created mobile view in "mon profil"

--- lib/views/profile/profile_view_mobile.dart
+++ lib/views/profile/profile_view_mobile.dart
@@ -1,9 +1,15 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:ape_manager_front/utils/font_utils.dart';
+import 'package:ape_manager_front/views/profile/prenom_field.dart';
+import 'package:ape_manager_front/views/profile/telephone_field.dart';
 import 'package:ape_manager_front/widgets/drawer_appli.dart';
 import 'package:ape_manager_front/widgets/header_appli.dart';
 import 'package:flutter/material.dart';
+
+import '../../proprietes/couleurs.dart';
+import 'email_field.dart';
+import 'nom_field.dart';
 
 class ProfileViewMobile extends StatelessWidget {
   const ProfileViewMobile({super.key});
@@ -16,9 +22,10 @@ class ProfileViewMobile extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          ImageAccueil(),
-          ParagraphePresentation(),
-          ParagrapheApplication(),
+          TextProfil(),
+          AllFields(),
+          SizedBox(height: 40,),
+          TableEnfants(),
         ],
       ),
       drawer: DrawerGlobal(),
@@ -26,106 +33,137 @@ class ProfileViewMobile extends StatelessWidget {
   }
 }
 
-class ImageAccueil extends StatelessWidget {
-  const ImageAccueil({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          height: 160,
-          width: double.infinity,
-          child: Image(
-            image: AssetImage("assets/images/ecole.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        Container(
-          height: 160,
-          child: Center(
-            child: Text(
-              "Bienvenue sur \n APE Manager",
-              textDirection: TextDirection.ltr,
-              style: FontUtils.getFontApp(
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ParagraphePresentation extends StatelessWidget {
-  const ParagraphePresentation({super.key});
+class TextProfil extends StatelessWidget {
+  const TextProfil({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(15.0),
-      child: Column(
-        children: [
-          // Titre "Qui sommes nous ?"
-          Padding(
-            padding: EdgeInsets.only(top: 10, bottom: 10),
+          height: 160,
+          child: Center(
             child: Text(
-              "Qui sommes nous ?",
+              "Mon Profil",
+              textDirection: TextDirection.ltr,
               style: FontUtils.getFontApp(
-                fontSize: 20,
+                color: Colors.black,
               ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(bottom: 10),
-            child: Text(
-              "Le bureau de l'APEL (Association des parents d'élèves de l'enseignement libre) est composé de six membres élus parmi les parents d'élèves. Tous les parents sont membres de l'APEL et sont les bienvenus aux réunions du CA.",
-              style: FontUtils.getFontApp(
-                fontSize: 15,
-                fontWeight: FontWeight.w200,
-              ),
-            ),
-          ),
+        );
+  }
+}
 
-          Text(
-            "L'APEL organise chaque année divers événements pour financer les activités des enfants, y compris des manifestations annuelles comme le marché de Noël et des opérations ponctuelles comme la vente de viennoiseries.",
-            style: FontUtils.getFontApp(
-              fontSize: 15,
-              fontWeight: FontWeight.w100,
+class AllFields extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      height: 350,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            NomField(),
+            SizedBox(
+              height: 10,
             ),
-          ),
-        ],
+            PrenomField(),
+            SizedBox(
+              height: 10,
+            ),
+            EmailField(),
+            SizedBox(
+              height: 10,
+            ),
+            TelephoneField(),
+          ],
+        ),
       ),
     );
   }
 }
 
-class ParagrapheApplication extends StatelessWidget {
-  const ParagrapheApplication({super.key});
+
+class TableEnfants extends StatefulWidget {
+  @override
+  _TableEnfantsState createState() => _TableEnfantsState();
+}
+
+class _TableEnfantsState extends State<TableEnfants> {
+  List<Child> children = [
+    Child(name: 'Doe', firstName: 'Johnny', classe: 'CP'),
+    Child(name: 'Doe', firstName: 'Johnny', classe: 'CP'),
+    Child(name: 'Doe', firstName: 'Johnny', classe: 'CP'),
+    Child(name: 'Doe', firstName: 'Johnny', classe: 'CP'),
+    Child(name: 'Doe', firstName: 'Johnny', classe: 'CP'),
+    Child(name: 'Doe', firstName: 'Johnny', classe: 'CP'),
+    Child(name: 'Doe', firstName: 'Johnny', classe: 'CP'),
+    Child(name: 'Doe', firstName: 'Johnny', classe: 'CP'),
+    Child(name: 'Doe', firstName: 'Johnny', classe: 'CP'),
+    Child(name: 'Doe', firstName: 'Johnny', classe: 'CP'),
+    Child(name: 'Doe', firstName: 'Johnny', classe: 'CP'),
+    Child(name: 'pipou', firstName: 'poupi', classe: 'CE4')
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(15.0),
-      child: Column(children: [
-        Text(
-          "Description de l'application",
-          style: FontUtils.getFontApp(
-            fontSize: 20,
-          ),
+      padding: EdgeInsets.symmetric(horizontal: 5),
+      height: 300,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: DataTable(
+          headingRowColor: MaterialStateProperty.all(BEIGE_FONCE),
+          columns: const [
+            DataColumn(
+                label: Text(
+                  'Nom',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )),
+            DataColumn(
+                label: Text(
+                  'Prénom',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )),
+            DataColumn(
+                label: Text(
+                  'Classe',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )),
+            DataColumn(
+                label: Text(
+                  '',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )),
+            DataColumn(
+                label: Text(
+                  '',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )),
+          ],
+          rows: children
+              .map((child) => DataRow(cells: [
+            DataCell(Text(child.name)),
+            DataCell(Text(child.firstName)),
+            DataCell(Text(child.classe)),
+            DataCell(Icon(Icons.edit), onTap: () {}),
+            DataCell(Icon(Icons.delete), onTap: () {
+              setState(() {
+                children.remove(child);
+              });
+            }),
+          ]))
+              .toList(),
         ),
-        Padding(
-          padding: EdgeInsets.only(top: 10, bottom: 10),
-          child: Text(
-            "L'application APE Manager vise à organiser des ventes éphémères dans le but de récolter de l'argent qui servira à l'école et aux enfants. Les parents et membres de l'association pourront commander via cette application.",
-            style: FontUtils.getFontApp(
-              fontSize: 15,
-              fontWeight: FontWeight.w200,
-            ),
-          ),
-        )
-      ]),
+      ),
     );
   }
+}
+
+class Child {
+  String name;
+  String firstName;
+  String classe;
+
+  Child({required this.name, required this.firstName, required this.classe});
 }
