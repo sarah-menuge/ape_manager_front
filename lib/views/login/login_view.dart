edbeabe39e1c219f5f57efa278fd9856da3eb7b0
Alexandre Pepin <pepin.alexandre4@gmail.com>
2024-02-19 12:43:34 +0100

    Responsive mobile + desktop (resize desktop)

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -12,29 +12,28 @@ class LoginView extends StatelessWidget {
     return Scaffold(
       backgroundColor: BEIGE_FONCE,
       body: Center(
-        // Div principale
-        child: FractionallySizedBox(
-          widthFactor:
-              ResponsiveConstraint.getResponsiveValue(context, 0.9, 0.6),
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10,
-              vertical: 20,
-            ),
-            decoration: BoxDecoration(
-              color: BEIGE_CLAIR,
-              borderRadius: BorderRadius.circular(8.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: const Offset(0, 3),
-                ),
-              ],
-            ),
-            child: LoginCard(),
+        child: Container(
+          constraints: const BoxConstraints(
+            maxWidth: 1000,
+            maxHeight: 600,
           ),
+          margin: const EdgeInsets.symmetric(
+            horizontal: 10,
+            vertical: 20,
+          ),
+          decoration: BoxDecoration(
+            color: BEIGE_CLAIR,
+            borderRadius: BorderRadius.circular(8.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: LoginCard(),
         ),
       ),
     );
