c3e1281df835a3997c8857ac66375215b5c80acc
florian_robe <florian_robe@ens.univ-artois.fr>
2024-02-23 22:23:45 +0100

    created popur for mot de passe oublié

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MainApp extends StatelessWidget {
     ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: ForgotPasswordView.routeName,
+      initialRoute: LoginView.routeName,
       onGenerateRoute: (settings) {
         if (settings.name == LoginView.routeName) {
           return MaterialPageRoute(builder: (_) => LoginView());
--- lib/views/login/forgot_password_button.dart
+++ lib/views/login/forgot_password_button.dart
@@ -1,7 +1,52 @@
-import 'package:ape_manager_front/utils/font_utils.dart';
+import 'package:ape_manager_front/proprietes/couleurs.dart';
 import 'package:flutter/material.dart';
+import 'package:ape_manager_front/utils/font_utils.dart';
 
 class ForgotPasswordButton extends StatelessWidget {
+  void _showForgotPasswordDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Stack(
+            children: <Widget>[
+              Form(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(maxWidth: 450),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.mail),
+                          ),
+                          style: const TextStyle(height: 1),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        child: Text("Envoyer"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -9,7 +54,9 @@ class ForgotPasswordButton extends StatelessWidget {
       child: Align(
         alignment: Alignment.centerRight,
         child: TextButton(
-          onPressed: () {},
+          onPressed: () {
+            _showForgotPasswordDialog(context);
+          },
           child: Text(
             'Mot de passe oublié ?',
             style: FontUtils.getFontApp(
