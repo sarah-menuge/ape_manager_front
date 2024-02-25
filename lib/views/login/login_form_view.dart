a0f393ce4e472bb80cd5ea6c7e6ca8702d130562
Alexandre Pepin <pepin.alexandre4@gmail.com>
2024-02-25 17:16:01 +0100

    Merge avec main

--- lib/views/login/login_form_view.dart
+++ lib/views/login/login_form_view.dart
@@ -144,7 +144,7 @@ class _LoginFormViewState extends State<LoginFormView> {
       child: Align(
         alignment: Alignment.centerRight,
         child: TextButton(
-          onPressed: () {},
+          onPressed: () {_showForgotPasswordDialog(context);},
           child: Text(
             'Mot de passe oublié ?',
             style: FontUtils.getFontApp(
@@ -204,6 +204,59 @@ class _LoginFormViewState extends State<LoginFormView> {
         backgroundColor: BLEU,
       ),
       child: const Text('Se connecter'),
+    );
+  }
+
+  void _showForgotPasswordDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: BEIGE_CLAIR.withOpacity(1.0),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text("Veuillez entrer votre mail pour demander une réinitialisation du mot de passe",style: FontUtils.getFontApp(fontSize: 15),),
+                SizedBox(height: 20,),
+                Form(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: ConstrainedBox(
+                          constraints: const BoxConstraints(maxWidth: 450),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(Icons.mail),
+                            ),
+                            style: const TextStyle(height: 1),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: BLEU,
+                          ),
+                          child: Text("Envoyer",style: TextStyle(color: BLANC),),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
