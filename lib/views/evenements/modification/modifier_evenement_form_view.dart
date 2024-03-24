4158bbc4442cc76e81fd90e44de3584e3fcd59f0
florian_robe <florian_robe@ens.univ-artois.fr>
2024-03-24 18:46:22 +0100

    corrected keyboard closing on event edit

--- lib/views/evenements/modification/modifier_evenement_form_view.dart
+++ lib/views/evenements/modification/modifier_evenement_form_view.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:ape_manager_front/models/evenement.dart';
 import 'package:ape_manager_front/views/evenements/modification/modifier_evenement_view.dart';
 import 'package:ape_manager_front/views/evenements/modification/popup_annuler_modifications.dart';
@@ -6,8 +8,6 @@ import 'package:ape_manager_front/widgets/formulaire/champ_date.dart';
 import 'package:ape_manager_front/widgets/formulaire/champ_string.dart';
 import 'package:ape_manager_front/widgets/formulaire/formulaire.dart';
 import 'package:ape_manager_front/widgets/formulaire/formulaire_state.dart';
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class ModifierEvenementFormView extends StatefulWidget {
   final Evenement evenement;
@@ -24,12 +24,16 @@ class ModifierEvenementFormView extends StatefulWidget {
   });
 
   @override
-  State<ModifierEvenementFormView> createState() =>
-      _ModifierEvenementFormViewState();
+  State<ModifierEvenementFormView> createState() => _ModifierEvenementFormViewState();
 }
 
-class _ModifierEvenementFormViewState
-    extends FormulaireState<ModifierEvenementFormView> {
+class _ModifierEvenementFormViewState extends FormulaireState<ModifierEvenementFormView> {
+  final Key titreChampKey = UniqueKey();
+  final Key descriptionChampKey = UniqueKey();
+  final Key dateDebutChampKey = UniqueKey();
+  final Key dateFinChampKey = UniqueKey();
+  final Key dateFinPaiementChampKey = UniqueKey();
+
   @override
   Formulaire setFormulaire(BuildContext context) {
     return Formulaire(
@@ -38,7 +42,7 @@ class _ModifierEvenementFormViewState
       champs: [
         [
           ChampString(
-            key: UniqueKey(),
+            key: titreChampKey,
             label: "Titre de l'événement",
             prefixIcon: const Icon(Icons.title),
             valeurInitiale: widget.evenement.titre,
@@ -48,7 +52,7 @@ class _ModifierEvenementFormViewState
         ],
         [
           ChampString(
-            key: UniqueKey(),
+            key: descriptionChampKey,
             label: "Description de l'événement",
             prefixIcon: const Icon(Icons.description),
             valeurInitiale: widget.evenement.description,
@@ -58,45 +62,42 @@ class _ModifierEvenementFormViewState
         ],
         [
           ChampDate(
-            key: UniqueKey(),
+            key: dateDebutChampKey,
             readOnly: widget.droitEvenement != DroitEvenement.modification,
             label: "Début des commandes",
             valeurInitiale: widget.evenement.getDateDebutString(),
             controller: widget.evenement.dateDebutTEC,
             onChangedMethod: (value) {
               try {
-                widget.evenement.dateDebut =
-                    DateFormat('dd-MM-yyyy').parse(value!);
+                widget.evenement.dateDebut = DateFormat('dd-MM-yyyy').parse(value!);
               } catch (e) {
                 widget.evenement.dateDebut = null;
               }
             },
           ),
           ChampDate(
-            key: UniqueKey(),
+            key: dateFinChampKey,
             readOnly: widget.droitEvenement != DroitEvenement.modification,
             label: "Fin des commandes",
             valeurInitiale: widget.evenement.getDateFinString(),
             controller: widget.evenement.dateFinTEC,
             onChangedMethod: (value) {
               try {
-                widget.evenement.dateFin =
-                    DateFormat('dd-MM-yyyy').parse(value!);
+                widget.evenement.dateFin = DateFormat('dd-MM-yyyy').parse(value!);
               } catch (e) {
                 widget.evenement.dateFin = null;
               }
             },
           ),
           ChampDate(
-            key: UniqueKey(),
+            key: dateFinPaiementChampKey,
             readOnly: widget.droitEvenement != DroitEvenement.modification,
             label: "Fin de paiement",
             valeurInitiale: widget.evenement.getDateFinPaiementString(),
             controller: widget.evenement.dateFinPaiementTEC,
             onChangedMethod: (value) {
               try {
-                widget.evenement.dateFinPaiement =
-                    DateFormat('dd-MM-yyyy').parse(value!);
+                widget.evenement.dateFinPaiement = DateFormat('dd-MM-yyyy').parse(value!);
               } catch (e) {
                 widget.evenement.dateFinPaiement = null;
               }
@@ -112,8 +113,7 @@ class _ModifierEvenementFormViewState
               showDialog(
                 context: context,
                 builder: (context) => PopupAnnulerModifications(
-                  annulerModificationsInfosGenerales:
-                      widget.annulerModificationsInfosGenerales,
+                  annulerModificationsInfosGenerales: widget.annulerModificationsInfosGenerales,
                 ),
               );
             },
